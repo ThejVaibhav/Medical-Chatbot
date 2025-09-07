9e1cfa70cca2d6bff7c078a31ab41a23e07af485
Vaibhav <thejvaibhav@gmail.com>
2025-09-08 00:29:12 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages, setup
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Vaibhav",
+    author_email="thejvaibhav@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
